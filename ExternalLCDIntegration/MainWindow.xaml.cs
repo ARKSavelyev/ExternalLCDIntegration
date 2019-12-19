0e0bbfc5437de14a828420326741f5b47e9cacfe
GuaXiN1M <GuaxinimCDO@gmail.com>
2019-12-19 16:46:54 +0000

    Screen Processing
    Added code for screen pixel processing

--- ExternalLCDIntegration/MainWindow.xaml.cs
+++ ExternalLCDIntegration/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace ExternalLCDIntegration
 {
@@ -25,6 +28,8 @@ namespace ExternalLCDIntegration
     {
         private BackgroundWorker _backgroundWorker;
         private bool _isRunning = false;
+        private int screenWidth;
+        private int screenHeight;
 
         public MainWindow()
         {
@@ -84,12 +89,71 @@ namespace ExternalLCDIntegration
 
         private void DoShit(int i)
         {
-            TextBox1.Text = i.ToString();
+            //var screen = new Bitmap(screenHeight,);
+            TextBox1.Text = $"Width: {screenWidth.ToString()} Height: {screenHeight.ToString()}";
+        }
+
+        private void PrintRGB(int avrB, int avrG, int avrR)
+        {
+            TextBox1.Text = $"R: {avrR.ToString()} G: {avrG.ToString()} B: {avrB.ToString()}";
+        }
+
+        private void GetAverageColor()
+        {
+            screenWidth = (int)Math.Floor(SystemParameters.PrimaryScreenWidth);
+            screenHeight = (int)Math.Floor(SystemParameters.PrimaryScreenHeight);
+            var totals = new long[] { 0, 0, 0 };
+            do
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                while (sw.ElapsedMilliseconds < 500)
+                {
+                    Thread.Sleep(100);
+                }
+                var screenBitmap = new Bitmap(screenWidth, screenHeight);
+                var format = screenBitmap.PixelFormat;
+                int bppModifier = format == System.Drawing.Imaging.PixelFormat.Format24bppRgb ? 3 : 4; // cutting corners, will fail on anything else but 32 and 24 bit images
+                var sourceData = screenBitmap.LockBits(new Rectangle(0, 0, screenWidth, screenHeight), ImageLockMode.ReadOnly,
+                    format);
+                var stride = sourceData.Stride;
+                var scan = sourceData.Scan0;
+
+                unsafe
+                {
+                    byte* p = (byte*)(void*)scan;
+
+                    for (int y = 0; y < screenHeight; y++)
+                    {
+                        for (int x = 0; x < screenWidth; x++)
+                        {
+                            for (int color = 0; color < 3; color++)
+                            {
+                                int idx = y * stride + x * bppModifier + color;
+                                totals[color] += p[idx];
+                                /*
+                                int idx = y * stride + x * bppModifier;
+                                totals[0] += p[idx];
+                                totals[1] += p[idx+1];
+                                totals[2] += p[idx+2];
+                                */
+                            }
+                        }
+                    }
+                }
+                int avgB = (int) (totals[0] / (screenWidth * screenHeight));
+                int avgG = (int) (totals[1] / (screenWidth * screenHeight));
+                int avgR = (int) (totals[2] / (screenWidth * screenHeight));
+
+                Dispatcher.BeginInvoke(new Action(() => { PrintRGB(avgB, avgG, avgR); }));
+            } while (_isRunning);
+
+            
         }
 
         private void BackgroundWorkerOnDoWork(object sender, DoWorkEventArgs e)
         {
-            DoTheLoop();
+            GetAverageColor();
+            //DoTheLoop();
             /*
             BackgroundWorker worker = (BackgroundWorker)sender;
             int i = 0;
